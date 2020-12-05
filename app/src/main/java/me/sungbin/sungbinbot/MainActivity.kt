5067a7b4dbaa3d51d49b4e9c382d722fc46dc8b1
sungbin5304 <sungbin.dev@gmail.com>
2020-12-05 16:24:16 +0900

    :eggplant: First commit! :eggplant:

---
+++ app/src/main/java/me/sungbin/sungbinbot/MainActivity.kt
@@ -0,0 +1,18 @@
+package me.sungbin.sungbinbot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import me.sungbin.sungbinbot.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
+}
