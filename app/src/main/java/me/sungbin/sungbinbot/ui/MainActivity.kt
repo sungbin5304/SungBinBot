ceffdbf8c0c76d366f7efb82b643f76d6d792142
sungbin5304 <sungbin.dev@gmail.com>
2020-12-08 11:46:05 +0900

    :cow: test :cow:

--- app/src/main/java/me/sungbin/sungbinbot/ui/MainActivity.kt
+++ app/src/main/java/me/sungbin/sungbinbot/ui/MainActivity.kt
@@ -1,8 +1,11 @@
 package me.sungbin.sungbinbot.ui
 
+import android.app.Notification
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import com.sungbin.androidutils.extensions.hide
 import com.sungbin.androidutils.extensions.show
 import com.sungbin.androidutils.util.DataUtil
@@ -14,9 +17,12 @@ import me.sungbin.sungbinbot.R
 import me.sungbin.sungbinbot.databinding.ActivityMainBinding
 import me.sungbin.sungbinbot.datastore.DataManager
 import me.sungbin.sungbinbot.util.PathManager
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var preLog: String
     private lateinit var binding: ActivityMainBinding
     private val bot = KakaoBot()
 
@@ -28,6 +34,8 @@ class MainActivity : AppCompatActivity() {
 
         bot.requestReadNotification()
 
+        binding.swPower.isChecked =
+            DataUtil.readData(applicationContext, PathManager.POWER, "false").toBoolean()
         binding.swPower.setOnCheckedChangeListener { _, isChecked ->
             DataUtil.saveData(applicationContext, PathManager.POWER, isChecked.toString())
             bot.setPower(isChecked)
@@ -38,8 +46,9 @@ class MainActivity : AppCompatActivity() {
                 .setTitle(getString(R.string.main_clear_log))
                 .setMessage(getString(R.string.main_question_clear_log))
                 .setPositiveButton(getString(R.string.delete)) { _, _ ->
-                    // todo
-                    hideLog()
+                    lifecycleScope.launchWhenCreated {
+                        DataManager.setLog("")
+                    }
                     ToastUtil.show(
                         applicationContext,
                         getString(R.string.main_cleared_log),
@@ -50,24 +59,47 @@ class MainActivity : AppCompatActivity() {
                 .show()
         }
 
+        DataManager.logFlow.asLiveData().observe(this) {
+            preLog = it
+            if (it.isEmpty()) {
+                hideLog()
+            } else {
+                binding.tvLog.text = it
+            }
+        }
+
         bot.setMessageReceiveListener { sender, message, room, isGroupChat, action, profileImage, packageName, bot ->
             with(message) {
                 when {
-                    contains(".살았니") -> bot.reply(action, "죽었다!")
+                    contains(".살았니") -> action.reply("죽었다!")
                 }
             }
         }
+    }
+
+    private fun Notification.Action.reply(message: String) {
+        bot.reply(this, message)
+        lifecycleScope.launchWhenCreated {
+            DataManager.setLog("$preLog\n${message.toLog()}")
+        }
+    }
+
+    private fun String.toLog(): String {
+        val format = SimpleDateFormat("M월 dd일 HH시 mm분 ss초", Locale.KOREA)
+        return "[${format.format(Date())}] $this"
     }
 
     private fun showLog() {
         binding.svLogContainer.show()
         binding.fblContainer.hide(true)
         binding.lavEmpty.cancelAnimation()
+        binding.fabClearLog.show()
     }
 
     private fun hideLog() {
         binding.svLogContainer.hide(true)
         binding.fblContainer.show()
         binding.lavEmpty.playAnimation()
+        binding.fabClearLog.hide()
     }
 }
